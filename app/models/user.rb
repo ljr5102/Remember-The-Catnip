9f2627c862aa35abd9688536f601f5b3e89effee
ljr5102 <ljr5102@gmail.com>
2016-03-29 11:53:39 -0400

    Create users table and add user model with validations and general user methods

---
+++ app/models/user.rb
@@ -0,0 +1,39 @@
+class User < ActiveRecord::Base
+
+  attr_reader :password
+
+  validates :username, :email_address, :session_token, uniqueness: true
+  validates :username, :email_address, :session_token, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    if user
+      return user if user.is_password?(password)
+    end
+    return nil
+  end
+
+  def password=(password)
+    if password.present?
+      @password = password
+      self.password_digest = BCrypt::Password.create(password)
+    end
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+end
