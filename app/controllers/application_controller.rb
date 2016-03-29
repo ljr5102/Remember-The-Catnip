72f549936361bcc37d6bffef58d9aa731fddd00b
ljr5102 <ljr5102@gmail.com>
2016-03-29 12:30:58 -0400

    Began auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,25 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user, :signed_in?
+
+  def login!(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def signed_in?
+    current_user
+  end
+
+  def logout!
+    if signed_in?
+      current_user.reset_session_token!
+      session[:session_token] = nil if session[:session_token]
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def authenticity_token
+    auth_token = <<-HTML
+    <input type="hidden" name="authenticity_token" value=#{form_authenticity_token}>
+    HTML
+    auth_token.html_safe
+  end
+
 end
