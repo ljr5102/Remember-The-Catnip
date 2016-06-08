b562aad04aec1a0521d227682a955db46fb643f4
ljr5102 <ljr5102@gmail.com>
2016-06-08 16:44:03 -0400

    Add task controller specs

--- spec/controllers/api/tasks_controller_spec.rb
+++ spec/controllers/api/tasks_controller_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Api::TasksController, type: :controller do
 
+  describe "routing" do
+    it { should route(:get, '/api/tasks').to(action: :index, format: :json) }
+    it { should route(:post, '/api/tasks').to(action: :create, format: :json) }
+    it { should route(:patch, '/api/tasks/1').to(action: :update, id: 1, format: :json) }
+    it { should route(:get, '/api/tasks/1').to(action: :show, id: 1, format: :json) }
+    it { should route(:delete, '/api/tasks/1').to(action: :destroy, id: 1, format: :json) }
+    it { should route(:patch, '/api/tasks/1/images').to(action: :remove_image, id: 1, format: :json) }
+
+    it { should route(:get, '/api/tasks/today').to(action: :today, format: :json) }
+    it { should route(:get, '/api/tasks/tomorrow').to(action: :tomorrow, format: :json) }
+    it { should route(:get, '/api/tasks/week').to(action: :week, format: :json) }
+    it { should route(:get, '/api/tasks/completed').to(action: :completed, format: :json) }
+  end
+
 end
